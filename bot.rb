9ab1cc5432ed3cb758d841f628b2beda76c1d790
Greg Bowser <topnotcher@gmail.com>
2013-10-10 21:35:47 -0400

    indentation (ruby indentation conention is broken and stupid)/change it to a derp bot

--- bot.rb
+++ bot.rb
@@ -1,32 +1,42 @@
 require 'cinch'
 
-# Give this bot ops in a channel and it'll auto voice
-# visitors
-#
-# Enable with !autovoice on
-# Disable with !autovoice off
+class TriviaTicker
+	include Cinch::Plugin
+
+	def initialize(*args)
+		super
+	end
+
+	timer 1, method: :tick
+	def tick
+		@bot.handlers.dispatch(:tick)
+	end
+end
 
 bot = Cinch::Bot.new do
-  configure do |c|
-    c.nick            = "cinch_autovoice"
-    c.server          = "irc.consental.com"
-    c.verbose         = true
-    c.channels        = ["#derp"]
 
-    @autovoice        = true
-  end
+	configure do |c|
+		c.nick = "derp"
+		c.server = "irc.consental.com"
+		c.verbose = true
+		c.channels = ["#derp"]
+		c.plugins.plugins = [TriviaTicker]
+	end
 
-  on :join do |m|
-    unless m.user.nick == bot.nick # We shouldn't attempt to voice ourselves
-      m.channel.voice(m.user) if @autovoice
-    end
-  end
+	on :join do |m|
+		#derp derp derp
+	end
 
-  on :channel, /^!autovoice (on|off)$/ do |m, option|
-    @autovoice = option == "on"
+	on :channel, /^!start$/ do |m|
+		@channel = m.channel
+		@active = true
 
-    m.reply "Autovoice is now #{@autovoice ? 'enabled' : 'disabled'}"
-  end
+	end
+
+	on :tick do
+		next unless @active
+		Channel(@channel).send 'derp'
+	end
 end
 
 bot.start
